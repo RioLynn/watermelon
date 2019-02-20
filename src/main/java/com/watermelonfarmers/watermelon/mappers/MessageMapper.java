88cc8c1241434bde8ac5e632e0d65f55a30cd835
RioLynn <wenqi.lin@uconn.edu>
2019-02-19 19:55:59 -0800

    Message implementation

---
+++ src/main/java/com/watermelonfarmers/watermelon/mappers/MessageMapper.java
@@ -0,0 +1,28 @@
+package com.watermelonfarmers.watermelon.mappers;
+
+import com.watermelonfarmers.watermelon.entities.MessageEntity;
+import com.watermelonfarmers.watermelon.models.Message;
+
+public class MessageMapper {
+    public static MessageEntity mapMessageEntityToMessage(Message request) {
+        MessageEntity messageEntity = new MessageEntity();
+        messageEntity.setId(request.getId());
+        messageEntity.setMessage(request.getMessage());
+        messageEntity.setCreated_by_user(request.getCreated_by_user());
+        messageEntity.setCreated(request.getCreated());
+        messageEntity.setLast_modified(request.getLast_modified());
+        messageEntity.setStatus(request.getStatus());
+        return messageEntity;
+    }
+
+    public static Message mapMessageToMessageEntity(MessageEntity messageEntity) {
+        Message message = new Message();
+        message.setId(messageEntity.getId());
+        message.setMessage(messageEntity.getMessage());
+        message.setCreated_by_user(messageEntity.getCreated_by_user());
+        message.setCreated(messageEntity.getCreated());
+        message.setLast_modified(messageEntity.getLast_modified());
+        message.setStatus(messageEntity.getStatus());
+        return message;
+    }
+}
